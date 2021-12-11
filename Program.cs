98cd31cc8255b72857e03f7ad80296eea8daae0e
gusus21 <yanenko.yurii1121@vu.cdu.edu.ua>
2021-12-11 14:44:36 +0200

    Start of the program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] b = new int[n];          
+            for (int i = 0; i < b.Length; i++)
+            {                
+                b[i] = int.Parse(Console.ReadLine());                
+            }
+            Console.WriteLine("Введений вами масив:");
+            for (int j = 0; j < b.Length; j++)
+            {
+                Console.WriteLine(b[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
